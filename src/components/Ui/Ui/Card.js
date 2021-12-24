18ba79ad40cb6e39afd7278161cc57196cbb08cc
sagorhasanmehedi <mehedihasansagor1995@gmail.com>
2021-12-24 11:53:37 +0600

    filter add

--- src/components/Ui/Ui/Card.js
+++ src/components/Ui/Ui/Card.js
@@ -4,7 +4,7 @@ import ExpencesItem from "../../Expences/ExpencesItem/ExpencesItem";
 import ExpencesFilter from "../../Expences/ExpencesFilter/ExpencesFilter";
 
 const Card = () => {
-  const [expencesYear, setExpencesYear] = useState(2020);
+  const [expencesYear, setExpencesYear] = useState("2020");
   const expenses = [
     {
       id: "e1",
@@ -16,7 +16,7 @@ const Card = () => {
       id: "e2",
       title: "New TV",
       amount: 799.49,
-      date: new Date(2021, 2, 12),
+      date: new Date(2019, 2, 12),
     },
     {
       id: "e3",
@@ -28,17 +28,22 @@ const Card = () => {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2020, 5, 12),
     },
   ];
 
   const YearHandel = (year) => {
     setExpencesYear(year);
   };
+
+  const selectedExpences = expenses.filter(
+    (item) => item.date.getFullYear().toString() === expencesYear
+  );
+
   return (
     <div className="Ui">
       <ExpencesFilter YearHandel={YearHandel} expencesYear={expencesYear} />
-      {expenses.map((Data) => (
+      {selectedExpences.map((Data) => (
         <ExpencesItem data={Data} key={Data.id} />
       ))}
     </div>
